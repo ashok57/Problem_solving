2741da8ce45d1a95849260919170ebc7981ea3c3
Ashok Kumar <68746723+ashok57@users.noreply.github.com>
2026-04-18 23:59:55 +0530

    Create reverse_array.java

---
+++ reverse_array.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class Main{
+    public static void main(String args[]){
+        int arr[] = {1,3,5,7};
+        
+        int start =0;
+        int end = arr.length-1;
+        
+        
+        while(start < end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end]  = temp;
+            
+            start++;
+            end--;
+            
+        }
+        System.out.println("Reversed array: " + Arrays.toString(arr));
+        
+    }
+}
+
