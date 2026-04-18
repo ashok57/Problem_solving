6db89028625ef10ba3813c47581271727201eb8d
Ashok Kumar <68746723+ashok57@users.noreply.github.com>
2026-04-18 23:47:33 +0530

    Create maximum_element.java

---
+++ maximum_element.java
@@ -0,0 +1,14 @@
+class Main {
+    public static void main(String[] args) {
+        
+        int[] arr = {3,7,2,9,5};
+        int max = arr[0];
+        
+        for(int i =0; i<arr.length; i++){
+            if(arr[i]> max){
+                max = arr[i];
+            }
+        }
+        System.out.println("Maximum element is " + max);
+    }
+}
